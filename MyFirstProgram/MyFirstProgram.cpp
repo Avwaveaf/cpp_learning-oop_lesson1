4011d4e9c454c1348dd8633e9bcf4c639101ce62
Afif <49422146+Avwaveaf@users.noreply.github.com>
2024-03-03 22:50:01 +0700

    creating safety and reusable template for dynamic array

--- MyFirstProgram/MyFirstProgram.cpp
+++ MyFirstProgram/MyFirstProgram.cpp
@@ -1,43 +1,56 @@
 #include <iostream>
 
-// dynamic array with heap allocation
-class IntArr
+
+
+/// <summary>
+/// In the previous example, we already done the dynamic array but fixed type to int
+/// now we need to implement generic so we dont have to create the same class
+/// for each different type for creating dynamic array
+/// </summary>
+template <typename T>
+class DynamicArray
 {
 	size_t m_size;
 	// declare array that store address
-	int * m_arr;
+	T * m_arr;
 
 public:
-	IntArr(size_t size)
+	DynamicArray(size_t size)
 		: m_size(size)
 		// storing array in heap using keyword new
 		// also new keyword will set default value to 0. so it will not giving a garbage
-		, m_arr(new int[size])
+		, m_arr(new T[size])
 	{
-		// however to guarantee the default initialization to 0 we need to use standard library
-		// which handle efficient initialization
 
-		// std::fill_n(<arr>, <size>, <value>)
-		std::fill_n(m_arr, size, 0);
 
 		std::cout << "array constructed\n";
 	}
 
 	// Destructor with cleanup, bcs we use pointer
-	~IntArr() 
+	~DynamicArray()
 	{
 		delete [] m_arr; 
 		std::cout << "Array deconstructed \n";
 	}
 
 	// setter and getter
-	int get(size_t index) const
+	T get(size_t index) const
 	{
+		// we need to handle out of bound error handling 
+		if (index >= m_size)
+		{
+			std::out_of_range("Index out of bounds");
+		}
 		return m_arr[index];
 	}
 
-	void set(size_t index, int val)
+	void set(size_t index, T val)
 	{
+		// we need to handle out of bound error handling 
+		if (index >= m_size)
+		{
+			std::out_of_range("Index out of bounds");
+		}
 		m_arr[index] = val;
 	}
 
@@ -56,11 +69,24 @@ public:
 int main()
 {
 	// define the size of 5
-	IntArr myArr(10);
+	// implementing the generic type
+
+	DynamicArray<int> myArr(10);
 
 	myArr.set(4, 7);
 
 	myArr.print();
+
+	std::cout << "string array:\n";
+	
+	DynamicArray<std::string> names(2);
+
+	names.set(0, "fdsfsddfs");
+
+	auto n = names.get(0);
+	std::cout << "\n\n" << n << "\n";
+	
+	names.print();
 	
 	return 0;
 
